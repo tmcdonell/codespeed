f4dca6d71dda6b0a31474702482fb4d4fff98067
Joachim Breitner <mail@joachim-breitner.de>
2014-07-13 11:45:49 +0200

    Pass benchmark metadata (e.g. units) along

--- tools/ghc/log2json.pl
+++ tools/ghc/log2json.pl
@@ -8,12 +8,34 @@ use File::Slurp;
 use JSON;
 use IPC::Run qw/run/;
 
+my $TIME = {
+	units_title => "Time",
+	units => "seconds",
+	lessisbetter => "True",
+};
+
+my $ALLOC = {
+	units_title => "Allocations",
+	units => "bytes",
+	lessisbetter => "True",
+};
+
+my $GOOD_TESTS = {
+	units_title => "Tests",
+	units => "tests",
+	lessisbetter => "False",
+};
+
+my $BAD_TESTS = {
+	units_title => "Tests",
+	units => "tests",
+	lessisbetter => "True",
+};
+
 for my $filename (@ARGV) {
 	my $output = dirname($filename)."/".basename($filename,".log"). ".json";
 
 	my $log = read_file($filename);
-
-	my %results;
 
 	my $commit;
 	if ($log =~ m/^commit ([a-f0-9]+)$/m) {
@@ -22,40 +44,47 @@ for my $filename (@ARGV) {
 		die "Coult not find commit ID in $filename\n";
 	}
 
-	$results{'testsuite/tests'}= $1
-		if ($log =~ m/^ +(\d+) total tests, which gave rise to/m);
-	$results{'testsuite/expected passes'} = $1
-		if ($log =~ m/^ +(\d+) expected passes/m);
-	$results{'testsuite/framework failures'} = $1
-		if ($log =~ m/^ +(\d+) caused framework failures/m);
-	$results{'testsuite/unexpected passes'} = $1
-		if ($log =~ m/^ +(\d+) unexpected passes/m);
-	$results{'testsuite/expected failures'} = $1
-		if ($log =~ m/^ +(\d+) expected failures/m);
-	$results{'testsuite/unexpected failures'} = $1
-		if ($log =~ m/^ +(\d+) unexpected failures/m);
-
-	my $out;
-	run (["nofib-analyse", "--csv=Allocs"], \$log, \$out) or die "cat: $?";
-	for (split /^/, $out) {
-		$results{"nofib/allocs/$1"} = $2 if /(.*),(.*)/;
-	}
-	run (["nofib-analyse", "--csv=Runtime"], \$log, \$out) or die "cat: $?";
-	for (split /^/, $out) {
-		$results{"nofib/runtime/$1"} = $2 if /(.*),(.*)/ and $2 > 1.0;
-	}
-
 	my $data = [];
-	while ( my ($key, $value) = each %results ) {
+	my $report = sub {
+		my ($unit, $name, $value) = @_;
 		push @$data, {
 			commitid => $commit,
 			project => "GHC",
 			branch => "default",
 			executable => "ghc",
 			environment => "nomeata's buildbot",
-			benchmark => $key,
+			benchmark => $name,
 			result_value => $value,
+			units_title => $unit->{units_title},
+			units => $unit->{units},
+			lessisbetter => $unit->{lessisbetter},
 		}
+
 	};
+
+	$report->($GOOD_TESTS, 'testsuite/tests', $1)
+		if ($log =~ m/^ +(\d+) total tests, which gave rise to/m);
+	$report->($GOOD_TESTS, 'testsuite/expected passes', $1)
+		if ($log =~ m/^ +(\d+) expected passes/m);
+	$report->($BAD_TESTS, 'testsuite/framework failures', $1)
+		if ($log =~ m/^ +(\d+) caused framework failures/m);
+	$report->($BAD_TESTS, 'testsuite/unexpected passes', $1)
+		if ($log =~ m/^ +(\d+) unexpected passes/m);
+	$report->($BAD_TESTS, 'testsuite/expected failures', $1)
+		if ($log =~ m/^ +(\d+) expected failures/m);
+	$report->($BAD_TESTS, 'testsuite/unexpected failures', $1)
+		if ($log =~ m/^ +(\d+) unexpected failures/m);
+
+	my $out;
+	run (["nofib-analyse", "--csv=Allocs"], \$log, \$out) or die "cat: $?";
+	for (split /^/, $out) {
+		$report->($ALLOC, "nofib/allocs/$1", $2)
+			if /(.*),(.*)/;
+	}
+	run (["nofib-analyse", "--csv=Runtime"], \$log, \$out) or die "cat: $?";
+	for (split /^/, $out) {
+		$report->($TIME, "nofib/time/$1", $2)
+			if /(.*),(.*)/ and $2 > 1.0;
+	}
 	write_file($output, to_json($data, {utf8 => 1, pretty => 1}));
 }
