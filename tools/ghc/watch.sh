0aad3b8528ab7b427b33d47c5ba2d1fa01a629f5
Joachim Breitner <mail@joachim-breitner.de>
2014-08-01 20:48:51 +0200

    Benchmark revisions in order (more reliable reports)

--- tools/ghc/watch.sh
+++ tools/ghc/watch.sh
@@ -7,7 +7,7 @@ cd ~/logs/
 while true
 do
 	(cd ghc-master; git pull)
-	for rev in $(cd ghc-master; git log --oneline --first-parent db19c665ec5055c2193b2174519866045aeff09a..HEAD | cut -d\  -f1)
+	for rev in $(cd ghc-master; git log --oneline --first-parent db19c665ec5055c2193b2174519866045aeff09a..HEAD | cut -d\  -f1 | tac)
 	do
 		if ! [ -e "$rev.log" -o  -e "$rev.log.broken" -o -d "ghc-tmp-$rev" ]
 		then
