aaddda2312bcb53c63a8b083e02ef25ad3112961
Joachim Breitner <mail@joachim-breitner.de>
2014-10-15 15:31:53 +0200

    Parameter --count to ./manage.py  recalculate_reports

--- codespeed/management/commands/recalculate_reports.py
+++ codespeed/management/commands/recalculate_reports.py
@@ -1,13 +1,24 @@
 from django.core.management.base import BaseCommand, CommandError
+from optparse import make_option
 from codespeed.models import Report
 
 
 class Command(BaseCommand):
     help = 'Recalculates all reports'
 
+    option_list = BaseCommand.option_list + (
+        make_option('--count',
+            action="store",
+            type="int",
+            ),
+        )
+
     def handle(self, *args, **options):
         n = 0
-        for report in Report.objects.all():
+        q = Report.objects.order_by('-revision__date')
+        if 'count' in options:
+            q = q[:options['count']]
+        for report in q:
             self.stdout.write('Recalculating report %s...' % report)
             report.save()
             n += 1
